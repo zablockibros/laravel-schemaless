1afc8b3626258e888b65717376be958b2ba45b67
Justin Zablocki <zablockijj@gmail.com>
2019-03-04 14:43:45 -0500

    Changes

--- src/Models/Item.php
+++ src/Models/Item.php
@@ -2,21 +2,16 @@
 
 namespace ZablockiBros\Schemaless\Models;
 
-use ZablockiBros\Schemaless\Contracts\ValidatesAttributes;
 use ZablockiBros\Schemaless\Traits\HasSchemalessAttributes;
 use ZablockiBros\Schemaless\Traits\HasSchemalessRelationships;
 use ZablockiBros\Schemaless\Traits\HasSchemalessTable;
-use ZablockiBros\Schemaless\Traits\ValidatesModelAttributes;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Item extends Model implements ValidatesAttributes
+class Item extends Model
 {
     use HasSchemalessTable;
     use HasSchemalessAttributes;
     use HasSchemalessRelationships;
-    use ValidatesModelAttributes;
-    use SoftDeletes;
 
     /**
      * @var array
